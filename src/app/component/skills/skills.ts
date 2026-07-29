4ba2e479f111fdebe420e8ade2fafbc483761a11
Aline Dobo <dobo.aline@gmail.com>
2026-07-29 11:51:15 +0200

    Skills Section: html for @for loop and Map (can't believe it works)

--- src/app/component/skills/skills.ts
+++ src/app/component/skills/skills.ts
@@ -6,4 +6,22 @@ import { Component } from '@angular/core';
   templateUrl: './skills.html',
   styleUrl: './skills.scss',
 })
-export class Skills {}
+
+
+export class Skills {
+    rowOne = new Map([
+    ["Agular", "assets/icons/Logo_angular.png"],
+    ["TypeScript", "assets/icons/Logo_typescript.png"],
+    ["JavaScript", "assets/icons/Logo_javascript.png"],
+    ["HTML", "assets/icons/Logo_html.png"],
+    ["CSS", "assets/icons/Logo_css.png"],
+  ]);
+
+  rowTwo = new Map([
+    ["Supabase", "assets/icons/Logo_supabase.png"],
+    ["Git", "assets/icons/Logo_git.png"],
+    ["Scrum", "assets/icons/Logo_scrum.png"],
+    ["REST-API", "assets/icons/Logo_api.png"],
+    ["Material Design", "assets/icons/Logo_materialdesign.png"],
+  ]);
+}

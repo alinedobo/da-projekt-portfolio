31cc195c18400afeff9db192047d1a62a8a38c77
Aline Dobo <dobo.aline@gmail.com>
2026-07-29 15:45:40 +0200

    Portfolio Section: Project dynamic with for loop

--- src/app/component/portfolio/portfolio.ts
+++ src/app/component/portfolio/portfolio.ts
@@ -6,4 +6,21 @@ import { Component } from '@angular/core';
   templateUrl: './portfolio.html',
   styleUrl: './portfolio.scss',
 })
-export class Portfolio {}
+export class Portfolio {
+  projectList = [
+    {
+      name: 'Join',
+      description:
+        'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+      asset: 'portfolio_join.png',
+      skills: 'Angular | TypeScript | CSS | HTML | Firebase'
+    },
+    {
+      name: 'El Pollo Loco',
+      description:
+        'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.',
+      asset: 'portfolio-ellpolloloco.png',
+      skills: ['JavaScript', 'CSS', 'HTML'],
+    },
+  ];
+}

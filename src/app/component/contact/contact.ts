d7107be94d5dcb79aed4382fe1f80167780f0de7
Aline Dobo <dobo.aline@gmail.com>
2026-07-30 15:27:47 +0200

    Contact Section: Form implemented, works (missinf styling and privacy check)

--- src/app/component/contact/contact.ts
+++ src/app/component/contact/contact.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
 })
-
-
 export class Contact {
+  fb = inject(FormBuilder);
 
+  contactform = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(4)]],
+    email: ['', [Validators.required, Validators.email]],
+    message:['', [Validators.required, Validators.minLength(50)]]
+  });
 
+  formSubmit() {
+    console.log(this.contactform.value);
+  }
+
+  formReset() {
+    this.contactform.reset();
+  }
 }
